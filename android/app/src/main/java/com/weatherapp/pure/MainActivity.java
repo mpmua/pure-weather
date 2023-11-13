f535ae85e666e3ab0c75e079f299f0b5a41a1eaf
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-11-13 09:31:41 +0000

    testing status bar colors

---
+++ android/app/src/main/java/com/weatherapp/pure/MainActivity.java
@@ -0,0 +1,5 @@
+package com.weatherapp.pure;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/js/global.js
+++ src/js/global.js
@@ -1,7 +1,30 @@
 import { StatusBar, Style } from "@capacitor/status-bar";
+import { SplashScreen } from "@capacitor/splash-screen";
 import { Capacitor } from "@capacitor/core";
+import rgbHex from "rgb-hex";
 
-if (Capacitor.getPlatform() === "android") {
-    
-    StatusBar.setOverlaysWebView({ overlay: true });
-}
+let bgColor = localStorage.getItem("bgcolor");
+console.log(bgColor);
+
+const startIndex = bgColor.indexOf("rgb(");
+const endIndex = bgColor.indexOf(")");
+const statusBarColor = bgColor.substring(startIndex, endIndex + 1);
+console.log(statusBarColor);
+console.log(rgbHex(statusBarColor));
+
+StatusBar.setBackgroundColor({ color: "#ffffff" });
+StatusBar.setStyle({ style: Style.Light });
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (Capacitor.isNativePlatform()) {
+    // StatusBar.setBackgroundColor({ color: rgbHex(statusBarColor) });
+    // StatusBar.setBackgroundColor({ color: "#ffffff" });
+    // StatusBar.setStyle({ style: Style.Light });
+
+    setTimeout(() => {
+      SplashScreen.hide({
+        fadeOutDuration: 250,
+      });
+    }, 500);
+  }
+});
