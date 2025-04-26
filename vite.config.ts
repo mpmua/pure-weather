5a8ecf4e44e11fa979a4d375404288a586408e3f
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-04-26 15:55:19 +0100

    fix: resolve issue with homepage not loading in some circumstances

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    root: "./src",
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "./src/index.html"),
-                settingsPage: resolve(__dirname, "./src/settings.html"),
-                // citiesJsonFile: resolve(__dirname, "src/assets/current_city_list.json")
-            }
-        },
-        outDir: "../dist",
-        minify: false,
-        emptyOutDir: true
-    }
+  root: "./src",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/index.html"),
+        settingsPage: resolve(__dirname, "./src/settings.html"),
+        citiesJsonFile: resolve(__dirname, "./src/js/current_city_list.json"),
+      },
+    },
+    outDir: "../dist",
+    minify: false,
+    emptyOutDir: true,
+  },
 });
